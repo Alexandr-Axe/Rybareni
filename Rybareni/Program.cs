8021020fd33d54fefb35d9af3d78246e46c245d7
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2021-04-14 15:29:00 +0200

    Přidejte soubory projektu.

---
+++ Rybareni/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rybareni
+{
+    class Program
+    {
+        static void Main()
+        {
+            int MapaX = 10;
+            int MapaY = 10;
+            int[,] Mapa = new int[MapaX, MapaY];
+            const int PocetRyb = 50;
+            int Ryba = 1;
+            Random rybaPozice = new Random();
+            int rX, rY;
+            for (int x = 0; x < PocetRyb; x++)
+            {
+                rX = rybaPozice.Next(0, MapaX);
+                rY = rybaPozice.Next(0, MapaY);
+                if (Mapa[rX, rY] == Ryba)
+                {
+                    while (Mapa[rX, rY] == Ryba)
+                    {
+                        rX = rybaPozice.Next(0, MapaX);
+                        rY = rybaPozice.Next(0, MapaY);
+                        if (Mapa[rX, rY] != Ryba)
+                        {
+                            Mapa[rX, rY] = Ryba;
+                            break;
+                        }
+                    }
+                }
+                else Mapa[rX, rY] = Ryba;
+            }
+            //VypisPole(Mapa);
+            //Loviste(Mapa, 10, 10);
+            //MaximalniLoviste(Mapa, 3, 3);
+        }
+        public static void VypisPole(int[,] Pole)
+        {
+            int poleX = Pole.GetLength(0);
+            int poleY = Pole.GetLength(1);
+            for (int x = 0; x < poleX; x++)
+            {
+                for (int y = 0; y < poleY; y++)
+                {
+                    Console.Write($"{Pole[x, y]} ");
+                }
+            }
+        }
+        public static void Loviste(int[,] Pole, int delkaX, int delkaY) 
+        {
+            Random nc = new Random();
+            int poleX = Pole.GetLength(0);
+            int poleY = Pole.GetLength(1);
+            int hraniceX = nc.Next(0, poleX - delkaX);
+            int hraniceY = nc.Next(0, poleY - delkaY);
+            if (poleX < delkaX || poleY < delkaY)
+                Console.WriteLine("CHYBA");
+            for (int x = hraniceX; x < hraniceX + delkaX; x++)
+            {
+                for (int y = hraniceY; y < hraniceY + delkaY; y++)
+                {
+                    if (Pole[x, y] == 1)
+                        Console.WriteLine($"Ryba na pozici {y + 1}:{x + 1}");
+                }
+            }
+        }
+        public static void MaximalniLoviste(int[,] Pole, int delkaX, int delkaY) 
+        {
+            int poziceX = 0;
+            int poziceY = 0;
+            int soucetRyb = 0;
+            int mezikrok;
+            int vysledneRyby = 0;
+            int poleX = Pole.GetLength(0);
+            int poleY = Pole.GetLength(1);
+            if (poleX < delkaX || poleY < delkaY)
+                Console.WriteLine("CHYBA");
+            for (int celkoveX = 0; celkoveX < poleX - delkaX; celkoveX++)
+            {
+                for (int celkoveY = 0; celkoveY < poleY - delkaY; celkoveY++)
+                {
+                    for (int x = 0; x < delkaX; x++)
+                    {
+                        for (int y = 0; y < delkaY; y++)
+                        {
+                            if (Pole[x + celkoveX, y + celkoveY] == 1)
+                                soucetRyb++;
+                        }
+                    }
+                    mezikrok = soucetRyb;
+                    soucetRyb = 0;
+                    if (mezikrok > vysledneRyby)
+                    {
+                        vysledneRyby = mezikrok;
+                        poziceX = celkoveX;
+                        poziceY = celkoveY;
+                    }
+                }
+            }
+            Console.WriteLine($"{vysledneRyby} ryb, levý horní roh je na pozici {poziceY}:{poziceX}, pravý dolní je na pozici {poziceY + delkaY - 1}:{poziceX + delkaX - 1}");
+        }
+    }
+}
---
+++ Rybareni/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("Rybareni")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rybareni")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("b4985e4d-965b-4b7d-84b3-7baa4df9f757")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
