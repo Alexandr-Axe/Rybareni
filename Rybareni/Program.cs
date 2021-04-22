f5774e80b8017935aba3d2b49f93e7672857eba4
Sekera Alexandr <sekera.alexandr.2017@skola.ssps.cz>
2021-04-22 23:32:59 +0200

    Fukční vytvoření pole a vyhození náhodného počtu ryb v poli 10x10

--- Rybareni/Program.cs
+++ Rybareni/Program.cs
@@ -10,100 +10,55 @@ namespace Rybareni
     {
         static void Main()
         {
-            int MapaX = 1000;
-            int MapaY = 1000;
-            int[,] Mapa = new int[MapaX, MapaY];
-            const int PocetRyb = 100000;
-            int Ryba = 1;
-            Random rybaPozice = new Random();
-            int rX, rY;
-            for (int x = 0; x < PocetRyb; x++)
-            {
-                rX = rybaPozice.Next(0, MapaX);
-                rY = rybaPozice.Next(0, MapaY);
-                if (Mapa[rX, rY] == Ryba)
-                {
-                    while (Mapa[rX, rY] == Ryba)
-                    {
-                        rX = rybaPozice.Next(0, MapaX);
-                        rY = rybaPozice.Next(0, MapaY);
-                        if (Mapa[rX, rY] != Ryba)
-                        {
-                            Mapa[rX, rY] = Ryba;
-                            break;
-                        }
-                    }
-                }
-                else Mapa[rX, rY] = Ryba;
-            }
-            //VypisPole(Mapa);
-            Loviste(Mapa, 10, 10);
-            MaximalniLoviste(Mapa, 30, 30);
+            Rybarime Santiago = new Rybarime(100, 100, 100_000);
+            Santiago.RandomFishArray();
+            //Santiago.BestFishArray(0, 0, 0);
         }
-        public static void VypisPole(int[,] Pole)
+        
+    }
+    public class Rybarime
+    {
+        public int VyskaRybnika { get; set; }
+        public int SirkaRybnika { get; set; }
+        public double PocetRyb { get; set; }
+        public double[,] Rybnik { get; set; }
+        public Rybarime(int vyskaRybnika, int sirkaRybnika, double pocetRyb) 
         {
-            int poleX = Pole.GetLength(0);
-            int poleY = Pole.GetLength(1);
-            for (int x = 0; x < poleX; x++)
-            {
-                for (int y = 0; y < poleY; y++)
-                {
-                    Console.Write($"{Pole[x, y]} ");
-                }
-            }
+            VyskaRybnika = vyskaRybnika;
+            SirkaRybnika = sirkaRybnika;
+            PocetRyb = pocetRyb;
+            Rybnik = new double[VyskaRybnika, SirkaRybnika];
         }
-        public static void Loviste(int[,] Pole, int delkaX, int delkaY) 
+        double hodnotaPole = 0d;
+        Random nc = new Random();
+        double[,] CreateFishArray()
         {
-            Random nc = new Random();
-            int poleX = Pole.GetLength(0);
-            int poleY = Pole.GetLength(1);
-            int hraniceX = nc.Next(0, poleX - delkaX);
-            int hraniceY = nc.Next(0, poleY - delkaY);
-            if (poleX < delkaX || poleY < delkaY)
-                Console.WriteLine("CHYBA");
-            for (int x = hraniceX; x < hraniceX + delkaX; x++)
+            for (int x = 0; x < Rybnik.GetLength(1); x++)
             {
-                for (int y = hraniceY; y < hraniceY + delkaY; y++)
+                for (int y = 0; y < Rybnik.GetLength(0); y++)
                 {
-                    if (Pole[x, y] == 1)
-                        Console.WriteLine($"Ryba na pozici {y + 1}:{x + 1}");
+                    Rybnik[x, y] = nc.NextDouble();
+                    hodnotaPole += Rybnik[x, y];
                 }
             }
+            PocetRyb *= hodnotaPole;
+            for (int x = 0; x < Rybnik.GetLength(1); x++)
+            {
+                for (int y = 0; y < Rybnik.GetLength(0); y++)
+                {
+                    Rybnik[x, y] *= PocetRyb;
+                }
+            }
+            return Rybnik;
         }
-        public static void MaximalniLoviste(int[,] Pole, int delkaX, int delkaY) 
+        public void RandomFishArray()
         {
-            int poziceX = 0;
-            int poziceY = 0;
-            int soucetRyb = 0;
-            int mezikrok;
-            int vysledneRyby = 0;
-            int poleX = Pole.GetLength(0);
-            int poleY = Pole.GetLength(1);
-            if (poleX < delkaX || poleY < delkaY)
-                Console.WriteLine("CHYBA");
-            for (int celkoveX = 0; celkoveX < poleX - delkaX; celkoveX++)
-            {
-                for (int celkoveY = 0; celkoveY < poleY - delkaY; celkoveY++)
-                {
-                    for (int x = 0; x < delkaX; x++)
-                    {
-                        for (int y = 0; y < delkaY; y++)
-                        {
-                            if (Pole[x + celkoveX, y + celkoveY] == 1)
-                                soucetRyb++;
-                        }
-                    }
-                    mezikrok = soucetRyb;
-                    soucetRyb = 0;
-                    if (mezikrok > vysledneRyby)
-                    {
-                        vysledneRyby = mezikrok;
-                        poziceX = celkoveX;
-                        poziceY = celkoveY;
-                    }
-                }
-            }
-            Console.WriteLine($"{vysledneRyby} ryb, levý horní roh je na pozici {poziceY}:{poziceX}, pravý dolní je na pozici {poziceY + delkaY - 1}:{poziceX + delkaX - 1}");
+            CreateFishArray();
+            Console.WriteLine(Convert.ToInt32((Rybnik[nc.Next(0, 101), nc.Next(0, 101)]) / hodnotaPole));
+        }
+        public void BestFishArray()
+        {
+            CreateFishArray();
         }
     }
 }
